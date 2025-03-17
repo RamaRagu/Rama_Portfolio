b125f24afe7eaa192dbcba78c2b53857bf122da7
RamaRagu <161216938+RamaRagu@users.noreply.github.com>
2025-03-17 18:47:03 +0530

    Add About section with personal introduction, skills, and GitHub activity

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Rama Raguram</span>
+            from <span className="purple"> Bhubaneswar, India.</span>
+            <br />
+            I am currently Completed my 2nd year in BSc in Computer Science at
+            University of Westminster - IIT.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Instruments
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Writing Tech Blogs
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+          <p style={{ color: "rgb(155 126 172)" }}> </p>"Build. Innovate. Leave
+          a mark. 🚀"
+          <br />
+          <br />
+          <footer className="blockquote-footer"> Rama Raguram</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
