76c4ddebefa8f4b2096cceea32d2b625bcc319fa
RamaRagu <161216938+RamaRagu@users.noreply.github.com>
2025-03-24 10:50:53 +0530

    Update AboutCard component to reflect new location

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,7 +9,7 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Rama Raguram</span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            from <span className="purple"> Colombo, SriLanka.</span>
             <br />
             I am currently Completed my 2nd year in BSc in Computer Science at
             University of Westminster - IIT.
